24d9d2d4443c051b9b71d071bc9bcc0482dfa04c
Tan Truong <congtan98@gmail.com>
2021-08-09 09:59:51 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Display from "./components/Display";
+import ButtonPanel from "./components/Panel";
+import calculate from "./logic/calculate";
+import "./App.css";
+
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  handleClick = buttonName => {
+    console.log("handleClick is called after clicking " + buttonName + " button.");
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    return (
+      <div className="component-app">
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
